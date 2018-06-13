45dd00c7c6fff577b354a296e71d30846dd72f1d
Albin N <albin.nilsson89@gmail.com>
2018-06-13 09:54:16 +0200

    remove examples

--- config/services.php
+++ config/services.php
@@ -2,9 +2,9 @@
 
 return [
     'unifaun' => [
-        'user_name' => env('UNIFAUN_USER', 'Your username'),
-        'group_name' => env('UNIFAUN_GROUP_NAME', 'Your group name'),
-        'password' => env('UNIFAUN_PASSWORD', 'Your password'),
+        'user_name' => env('UNIFAUN_USER'),
+        'group_name' => env('UNIFAUN_GROUP_NAME'),
+        'password' => env('UNIFAUN_PASSWORD'),
         'wsdl' => env('UNIFAUN_WSDL', 'https://service.apport.net:443/ws/services/ConsignmentWS?wsdl')
     ]
 ];
